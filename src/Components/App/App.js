794a2563e18f1331ec901b43a7d26a47267606f5
Mousa Ayoubi <mousaayoubi@gmail.com>
2019-12-11 17:57:09 +0200

    Update files

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
 
 class App extends React.Component {
   render() {
     return (
-    <h1>hi world</h1>
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+          <div className="App">
+            <SearchBar />
+          <div className="App-playlist">
+            <SearchResults />
+            <Playlist />
+          </div>
+        </div>
+      </div>
     )}
 }
 
