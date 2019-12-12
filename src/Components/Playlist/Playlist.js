bcf53e92377083c0be28492b232b23212a6903e5
Mousa Ayoubi <mousaayoubi@gmail.com>
2019-12-12 17:05:39 +0200

    Update files

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import './Playlist.css';
-import TrackList from '../TrackList/TrackList';
 
 class Playlist extends React.Component {
     render() {
         return (
             <div className="Playlist">
                 <input defaultValue={"New Playlist"}/>
-                {<TrackList />}
+                
                 <button className="Playlist-save">SAVE TO SPOTIFY</button>
             </div>
         )}
--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -3,14 +3,15 @@ import './TrackList.css';
 import Track from '../Track/Track';
 
 class TrackList extends React.Component {
-    render() {
-        return (
-            <div className="TrackList">
-                <Track />
-                <Track />
-                <Track />
-            </div>
-        )}
-};
+  render() {
+    return (
+      <div className="TrackList">
+        {this.props.tracks.map((track) => {
+            return <Track track={track} key={track.id}/>
+        })}
+      </div>
+    );
+  }
+}
 
 export default TrackList;
