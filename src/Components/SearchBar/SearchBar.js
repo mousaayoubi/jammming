c3a4f1f6e21a765e21085bd1c8cdd99fa4ae4ce5
Mousa Ayoubi <mousaayoubi@gmail.com>
2019-12-16 19:17:17 +0200

    Update files

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -20,7 +20,7 @@ class SearchBar extends React.Component {
     render() {
         return (
             <div className="SearchBar">
-                <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange} onfocus="this.value=''"/>
+                <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange} />
                 <button className="SearchButton" onClick={this.search}>SEARCH</button>
             </div>
         )}
